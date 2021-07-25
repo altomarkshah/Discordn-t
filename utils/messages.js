d5a697f7020bdeeb438ff5e3f8c94b5643fdb186
altomarkshah <87883318+altomarkshah@users.noreply.github.com>
2021-07-25 15:08:24 -0400

    Update messages.js

--- utils/messages.js
+++ utils/messages.js
@@ -5,7 +5,7 @@ function formatMessage(username, text) {
     return{
         username,
         text,
-        time: moment.utc().format('h:mm a')
+        time: moment.utc().format('h:mm a').unix()
     }
 
 }
